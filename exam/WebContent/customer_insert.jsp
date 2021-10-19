f39651e43cd1d1cf850dee419742837ff930dda9
Austin <austiny@snu.ac.kr>
2021-10-19 15:40:19 +0900

    xxxx

---
+++ exam/WebContent/customer_insert.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form name="customer_form" method="post"
+		action="customer_insert_db.jsp">
+		<table>
+			<caption>회원 정보 입력</caption>
+			<%@ include file="../common/html_input1.inc"%>
+
+			<%@ include file="../common/html_input2.inc"%>
+
+<tr>
+<td colspan="2" style="text-align:center" ><input type="submit" value="회원가입"></td>
+		</table>
+	</form>
+</body>
+</html>
--- exam/src/dbconnclose/DbConnClose.java
+++ exam/src/dbconnclose/DbConnClose.java
@@ -1,52 +1,57 @@
-package dbconnclose;
+package dbconnclose;         // 패키지이름(소문자)
 
 import java.sql.*;
 
-public class DbConnClose {
+public class DbConnClose {   // 클래스 이름(대문자)
 
-	public static Connection getConnection() {
-		//JDBC 드라이버 로딩
-		String driverClass = "org.mariadb.jdbc.Dirver";
+   public static Connection getConnection() {
+
+      // JDBC 드라이버 로딩(loading JDBC driver)
+      String driverClass = "org.mariadb.jdbc.Driver";
+
+      try {
+         Class.forName(driverClass);
+      } catch (ClassNotFoundException err) {
+         System.out.println("JDBC 드라이버 로딩 오류! " + err.getMessage());
+      }	
+
+      // MariaDB 서버와 데이터베이스 연결(connect server & database )
+      String url = "jdbc:mariadb://localhost:3306/shopmall";
+      String id = "root";      // DB 사용자 아이디
+      String pw = "admin";     // DB 사용자 패스워드
+      
+      Connection conn = null;
+	       
+      try {
+         conn = DriverManager.getConnection(url, id, pw);
+      } catch (SQLException sqlerr) {
+    	 System.out.println("데이터베이스 연결 오류! " + sqlerr.getMessage());
+      }
+      return conn;
+   }	
 		
-		try {
-			Class.forName(driverClass);
-		} catch (ClassNotFoundException err) {
-			System.out.println("JDBC 드라이버 오류! ");
-		}
-		// database  연결
-		String url = "jdbc:mariadb://localhost:3307/univ";
-		String id = "root";
-		String pw = "0000";
-		
-		Connection conn = null;
-		
-		try {
-			conn = DriverManager.getConnection(url,id,pw);
-		} catch (SQLException sqlerr) {
-			System.out.println("데이터베이스 연결 오류!");
-		}
-		return conn;
-	}
-	
-	//데이터 베이스 연결 종료
-	public static void resourceClose(ResultSet rset, Statement pstmt, Connection conn) {
-		
-		try {
-			if(rset != null) rset.close();
-			if(pstmt != null) pstmt.close();
-			if(conn != null) conn.close();
-		} catch (SQLException sqlerr) {
-			System.out.println("데이베이스 종료 오류!");
-		}
-	}
-	
-	public static void resourceClose(Statement pstmt, Connection conn) {
-		
-		try {
-			if(pstmt != null) pstmt.close();
-			if(conn != null) conn.close();
-		} catch (SQLException sqlerr) {
-			System.out.println("데이베이스 종료 오류!");
-		}
-	}	
+   // 데이터베이스 연결 종료(close database) - ResultSet, Statement, Connection 순
+   public static void resourceClose(ResultSet rset, Statement stmt, Connection conn) {
+
+      try {
+         if (rset != null) rset.close();
+         if (stmt != null) stmt.close();
+         if (conn != null) conn.close();
+      } catch (SQLException sqlerr) {
+     	 System.out.println("데이터베이스 종료 오류! " + sqlerr.getMessage());
+      }	   
+
+   }
+   
+   public static void resourceClose(Statement stmt, Connection conn) {
+
+	      try {
+	         if (stmt != null) stmt.close();
+	         if (conn != null) conn.close();
+	      } catch (SQLException sqlerr) {
+	     	 System.out.println("데이터베이스 종료 오류! " + sqlerr.getMessage());
+	      }	   
+
+	   }   
+
 }
